390860e4589c1c67461744d66a0c12a80f869179
seokimun <gimun08@naver.com>
2025-03-02 23:05:09 +0900

    refactor: 🔧 프리티어 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,35 +7,35 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validationSchema: Joi.object({
-        ENV: Joi.string().valid('dev', 'prod').required(),
-        DB_TYPE: Joi.string().required(),
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.number().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
-        DB_DATABASE: Joi.string().required(),
-      }),
-    }),
-    TypeOrmModule.forRootAsync({
-      useFactory: (env: ConfigService) => ({
-        type: env.get<string>('DB_TYPE') as 'postgres',
-        host: env.get<string>('DB_HOST'),
-        port: env.get<number>('DB_PORT'),
-        username: env.get<string>('DB_USERNAME'),
-        password: env.get<string>('DB_PASSWORD'),
-        database: env.get<string>('DB_DATABASE'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
-    MovieModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validationSchema: Joi.object({
+                ENV: Joi.string().valid('dev', 'prod').required(),
+                DB_TYPE: Joi.string().required(),
+                DB_HOST: Joi.string().required(),
+                DB_PORT: Joi.number().required(),
+                DB_USERNAME: Joi.string().required(),
+                DB_PASSWORD: Joi.string().required(),
+                DB_DATABASE: Joi.string().required(),
+            }),
+        }),
+        TypeOrmModule.forRootAsync({
+            useFactory: (env: ConfigService) => ({
+                type: env.get<string>('DB_TYPE') as 'postgres',
+                host: env.get<string>('DB_HOST'),
+                port: env.get<number>('DB_PORT'),
+                username: env.get<string>('DB_USERNAME'),
+                password: env.get<string>('DB_PASSWORD'),
+                database: env.get<string>('DB_DATABASE'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: true,
+            }),
+            inject: [ConfigService],
+        }),
+        MovieModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
