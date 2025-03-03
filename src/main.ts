797ba13c443de4349919ecbafe8f899d1b3ea9d5
seokimun <gimun08@naver.com>
2025-03-03 20:47:06 +0900

    refactor: 🔧 프리티어 적용 및 common폴더안에 base엔터티 이동

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+        }),
+    );
 
-  await app.listen(3000);
+    await app.listen(3000);
 }
 bootstrap();
